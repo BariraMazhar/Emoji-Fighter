89ca4e53b5d231a20f201c6a68f51bcc91b17056
BariraMazhar <124186761+BariraMazhar@users.noreply.github.com>
2025-11-25 00:23:48 +0500

    Emoji fighter

---
+++ index.js
@@ -0,0 +1,13 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function fighter() {
+    // Challenge:
+    // When the user clicks on the "Pick Fighters" button, pick two random 
+    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    let randomindexOne = Math.floor(Math.random() * fighters.length)  
+    let randomindexTwo = Math.floor(Math.random() * fighters.length)
+    stageEl.textContent = (fighters[randomindexOne] + " VS " + fighters[randomindexTwo]) 
+})
